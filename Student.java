7049a7f11c40010f1fbd4bf5f322322320bdf2a1
suhanajava <iamsmandal05@gmail.com>
2026-04-09 12:53:39 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,36 +1,45 @@
+import java.util.Scanner;
+interface Exam {
+    void percent_cal();
+} 
 class Student {
     String name;
-    int age;
-
-    Student() {
-        name = "Unknown";
-        age = 0;
+    int roll_no;
+    float marks1, marks2;
+    void getData(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        name = sc.nextLine();
+        System.out.println("Enter Roll No: ");
+        roll_no = sc.nextInt();
+        System.out.println("Enter Marks1 : ");
+        marks1 = sc.nextFloat();
+        System.out.println("Enter Marks2 : ");
+        marks2 = sc.nextFloat();
     }
-
-    Student(String n) {
-        name = n;
-        age = 0;
-    }
-
-    Student(String n, int a) {
-        name = n;
-        age = a;
-    }
-
-    
-    void display() {
-        System.out.println("Name: " + name + ", Age: " + age);
+    void show(){
+        System.out.println("\n--- Student Details ---");
+        System.out.println("Name: "+ name);
+        System.out.println("Roll No: "+roll_no);
+        System.out.println("Marks1: "+marks1);
+        System.out.println("Marks2: "+marks2);
     }
 }
-
- class Main {
-    public static void main(String[] args) {
-        
-        Student s1 = new Student();               
-        Student s2 = new Student("Rahul");        
-        Student s3 = new Student("Suhana", 21);    
-        s1.display();
-        s2.display();
-        s3.display();
+class Result extends Student implements Exam{
+    float per;
+    public void percent_cal(){
+        per = (marks1 + marks2) / 2;
+    }
+    void display(){
+        System.out.println("Percentage: "+per + "%");
+    }
+}
+class Main{
+    public static void main(String[] args){
+        Result r = new Result();
+        r.getData();
+        r.show();
+        r.percent_cal();
+        r.display();
     }
 }
